2c8c1553755e76e6de4585a22bdc1165969dd550
Gbenga Awodokun <gbenga.awodokun@ge.com>
2018-05-14 09:19:07 -0400

    stack and queue

---
+++ queue.py
@@ -0,0 +1,18 @@
+class queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, val):
+        self.queue.insert(0, val)
+
+    def dequeue(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.queue.pop()
+
+    def size(self):
+        return len(self.queue)
+
+    def is_empty(self):
+        return self.size() == 0
