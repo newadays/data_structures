8f76f73338dd6b13d5d61d837de6cdd9f954791f
Gbenga Awodokun <gbenga.awodokun@ge.com>
2018-05-15 15:10:13 -0400

    stack examples

--- queue_str.py
+++ queue_str.py
@@ -1,18 +1,55 @@
-class queue:
-    def __init__(self):
-        self.queue = []
+# class queue:
+#     def __init__(self):
+#         self.queue = []
+#
+#     def enqueue(self, val):
+#         self.queue.insert(0, val)
+#
+#     def dequeue(self):
+#         if self.is_empty():
+#             return None
+#         else:
+#             return self.queue.pop()
+#             #
+#
+#     def size(self):
+#         return len(self.queue)
+#
+#     def is_empty(self):
+#         return self.size() == 0
+#
+# A = "[1,4,5,10,2"
+# q = queue()
+# for i in A:
+#     q.enqueue(i)
+#
+# print(q.size())
 
-    def enqueue(self, val):
-        self.queue.insert(0, val)
 
-    def dequeue(self):
-        if self.is_empty():
-            return None
-        else:
-            return self.queue.pop()
+class Solution:
+    # @param A : list of strings
+    # @return an integer
+    def evalRPN(self, A):
+        stack = []
+        for ch in A:
+            if ch in "+-*/":
+                arg2 = stack.pop()
+                arg1 = stack.pop()
 
-    def size(self):
-        return len(self.queue)
+                if ch is "+":
+                    stack.append(arg1 + arg2)
+                if ch is "-":
+                    stack.append(arg1 - arg2)
+                if ch is "*":
+                    stack.append(arg1 * arg2)
+                if ch is "/":
+                    stack.append(arg1 // arg2)
+            else:
+                stack.append(int(ch))
 
-    def is_empty(self):
-        return self.size() == 0
+        return stack.pop()
+
+
+A = [ "5", "1", "2", "+", "4", "*", "+", "3", "-" ]
+Sols = Solution()
+print(Sols.evalRPN(A))
