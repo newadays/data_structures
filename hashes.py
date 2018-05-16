45af70bfed69da38e648649fd7d219a31df5038e
Gbenga Awodokun <gbenga.awodokun@ge.com>
2018-05-16 17:42:21 -0400

    hash examples

--- hashes.py
+++ hashes.py
@@ -70,14 +70,42 @@ class Solution:
         else:
             return []
 
+    def equal(self, A):
+        lst = {}
+        result = []
+        n = len(A)
+        for a in range(n):
+            for b in range(a + 1, n):
+                cur_sum = A[a] + A[b]
+                seen_numbers = {}
+                possible_c_d = []
 
-A = [34, 63, 64, 38, 65, 83, 50, 44, 18, 34, 71, 80, 22, 28, 20, 96, 33, 70, 0, 25, 64, 96, 18, 2, 53, 100, 24, 47, 98,
-     69, 60, 55, 8, 38, 72, 94, 18, 68, 0, 53, 18, 30, 86, 55, 13, 93, 15, 43, 73, 68, 29]
+                for d in range(a + 1, n):
+                    if cur_sum - A[d] in seen_numbers:
+                        c = seen_numbers[cur_sum - A[d]]
+                        if b != c and b != d:
+                            possible_c_d.append([c, d])
+                        # else:
+                        # print("didn't return " + str([a, b, c, d]))
+                    elif A[d] not in seen_numbers and b != d:
+                        seen_numbers[A[d]] = d
+
+                if len(possible_c_d) > 0:
+                    c, d = (list(sorted(possible_c_d)))[0]
+                    return ([a, b, c, d])
+
+
+# A = [34, 63, 64, 38, 65, 83, 50, 44, 18, 34, 71, 80, 22, 28, 20, 96, 33, 70, 0, 25, 64, 96, 18, 2, 53, 100, 24, 47, 98,
+#      69, 60, 55, 8, 38, 72, 94, 18, 68, 0, 53, 18, 30, 86, 55, 13, 93, 15, 43, 73, 68, 29]
 Solution = Solution()
-print(Solution.twoSum(A, 25))
-print(Solution.diffPossible([2, 7, 11, 15], 9))
-# S = "barfoothefoobarman"
-# L = ["foo", "bar"]
-S = "abbaccaaabcabbbccbabbccabbacabcacbbaabbbbbaaabaccaacbccabcbababbbabccabacbbcabbaacaccccbaabcabaabaaaabcaabcacabaa"
-L = [ "cac", "aaa", "aba", "aab", "abc" ]
-print(Solution.findSubstring(S, L))
+# print(Solution.twoSum(A, 25))
+# print(Solution.diffPossible([2, 7, 11, 15], 9))
+# # S = "barfoothefoobarman"
+# # L = ["foo", "bar"]
+# S = "abbaccaaabcabbbccbabbccabbacabcacbbaabbbbbaaabaccaacbccabcbababbbabccabacbbcabbaacaccccbaabcabaabaaaabcaabcacabaa"
+# L = [ "cac", "aaa", "aba", "aab", "abc" ]
+# print(Solution.findSubstring(S, L))
+# x = Solution()
+print(Solution.equal([ 3, 4, 7, 1, 2, 9, 8  ]))
+
+
