a261abe9b5ec0d73ea020ac604f85a608b924c57
Gbenga Awodokun <gbenga.awodokun@ge.com>
2018-05-24 01:12:39 -0400

    add str examples

--- LinkedList.py
+++ LinkedList.py
@@ -1,97 +1,212 @@
-# # #   You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
-# #
-# # # Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-# # # Output: 7 -> 0 -> 8
-# #
-# # #  342 + 465 = 807
-# # # Make sure there are no trailing zeros in the output list
-# # # So, 7 -> 0 -> 8 -> 0 is not a valid response even though the value is still 807.
-# #
-# # # Definition for singly-linked list.
-#
-#
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-#
-#
-# class Solution:
-#     # @param A : head node of linked list
-#     # @param B : head node of linked list
-#     # @return the head node in the linked list
-#
-#     def addTwoNumbers(self, A, B):
-#         head = ListNode(0)
-#         pointer = head
-#         carry_over = 0
-#         while A and B:
-#             s = A.val + B.val + carry_over
-#             pointer.next = ListNode(s % 10)
-#             pointer = pointer.next
-#             carry_over = int(s/10)
-#             A = A.next
-#             B = B.next
-#
-#         if A is None:
-#             while B:
-#                 s = B.val + carry_over
-#                 pointer.next = ListNode(s % 10)
-#                 pointer = pointer.next
-#                 carry_over = int(s/10)
-#                 B = B.next
-#
-#         if B is None:
-#             while A:
-#                 s = A.val + carry_over
-#                 pointer.next = ListNode(s % 10)
-#                 pointer = pointer.next
-#                 carry_over = int(s/10)
-#                 A = A.next
-#
-#         if carry_over > 0:
-#             pointer.next = ListNode(carry_over)
-#
-#         return head.next
+#  You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+
+#  342 + 465 = 807
+# Make sure there are no trailing zeros in the output list
+# So, 7 -> 0 -> 8 -> 0 is not a valid response even though the value is still 807.
+
+# Definition for singly-linked list.
 
 
-# @param A : head node of linked list
-# @param B : integer
-# @param C : integer
-# @return the head node in the linked
-def reverseBetween(head, m, n):
-    def reverse(head, m, n):
-        c = m
-        tail = None
-        prev, curr = None, head
-        while c <= n and curr:
-            c += 1
-            temp = curr.next
-            curr.next = prev
-            if tail is None:
-                tail = curr
+class Node:
+    def __init__(self, val=None):
+        self.val = val
+        self.next = None
+
+    def get_node(self, k):
+        current = self.next
+        i = 0
+        while i < k:
+            current = current.next
+            i += 1
+        return current
+
+    def insert_node(self, val):
+        new_node = Node(val)
+        new_node.next = self.next
+        self.next = new_node
+
+    def delete_node(self, val):
+        if self.next.val == val:
+            self.next = self.next.next
+        else:
+            curr = self.next
+            while curr:
+                if curr.next and curr.next.val == val:
+                    curr.next = curr.next.next
+                curr = curr.next
+
+    def insert_kth_node(self, k, val):
+        curr = self.next
+        prev = None
+        i = 1
+        while curr:
+
+            if i == k:
+                new_node = Node(val)
+                prev.next = new_node
+                new_node.next = curr
+                break
+            i += 1
             prev = curr
-            curr = temp
-        return prev, tail, curr
+            curr = curr.next
 
-    if not head or not head.next:
-        return head
+    def delete_kth_node(self, k):
+        if k > 0 and self:
+            if k == 1:
+                # head node
+                self.next = self.next.next
+            else:
+                curr = self.next
+                i = 1
+                # delete head to be handled later
+                while curr:
+                    i += 1
 
-    c = 1
-    prev, curr = None, head
-    while c < m and curr:
-        c += 1
-        prev, curr = curr, curr.next
+                    if i == k:
+                        curr.next = curr.next.next
 
-    start, end, after = reverse(curr, c, n)
-    if end:
-        end.next = after
-    if prev:
-        prev.next = start
-    else:
-        head = start
-    return head
+                    curr = curr.next
 
+    def reverse(self):
+        prev = None
+        curr = self.next
+
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+
+        self.next = prev
+
+    def reverseBtw(self, m, n):
+        # case 1
+        # LL btw m and n
+        # case 2
+        # LL ==n(head) but m less than tail
+        # LL n shorter than (head) but m == tail
+        # @param A : head node of linked list
+        # @param B : integer
+        # @param C : integer
+        # @return the head node in the linked
+
+        def reversemn(curr_node, i, j):
+            next = None
+            tail_node = None
+            prev_node = None
+            while i <= j and curr_node:
+                i += 1
+                next = curr_node.next
+                curr_node.next = prev_node
+                if tail_node is None:
+                    tail_node = curr_node
+
+                prev_node = curr_node
+                curr_node = next
+
+            return prev_node, tail_node, curr_node
+
+        k = 1
+        prev = None
+        curr = self.next
+        while k < m and curr:
+            prev = curr
+            curr = curr.next
+            k += 1
+
+        start, end, after = reversemn(curr, k, n)
+
+        if end:
+            end.next = after
+
+        if prev:
+            prev.next = start
+        else:
+            self.next = start
+
+        def deleteDuplicates(self, A):
+            head = A
+            while A:
+                while A.next and A.next.val == A.val:
+                    A.next = A.next.next
+                A = A.next
+            return head
+
+
+head = Node()
+
+head.insert_node(11)
+head.insert_node(12)
+head.insert_node(31)
+head.insert_node(41)
+head.insert_node(51)
+
+# head.delete_node(4)
+
+head.insert_kth_node(2, 32)
+
+# head.delete_kth_node(1)
+curr = head.next
+
+while curr:
+    print(curr.val)
+    curr = curr.next
+
+head.reverseBtw(2, 4)
+
+curr = head.next
+
+while curr:
+    print(curr.val)
+    curr = curr.next
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    # @param A : head node of linked list
+    # @param B : head node of linked list
+    # @return the head node in the linked list
+
+    def addTwoNumbers(self, A, B):
+        head = ListNode(0)
+        pointer = head
+        carry_over = 0
+        while A and B:
+            s = A.val + B.val + carry_over
+            pointer.next = ListNode(s % 10)
+            pointer = pointer.next
+            carry_over = int(s/10)
+            A = A.next
+            B = B.next
+
+        if A is None:
+            while B:
+                s = B.val + carry_over
+                pointer.next = ListNode(s % 10)
+                pointer = pointer.next
+                carry_over = int(s/10)
+                B = B.next
+
+        if B is None:
+            while A:
+                s = A.val + carry_over
+                pointer.next = ListNode(s % 10)
+                pointer = pointer.next
+                carry_over = int(s/10)
+                A = A.next
+
+        if carry_over > 0:
+            pointer.next = ListNode(carry_over)
+
+        return head.next
 
 # @param A : head node of linked list
 # @param B : head node of linked list
@@ -131,153 +246,6 @@ def getIntersectionNode(A, B):
                 B = B.next
 
     return None
-#
-# #LinkedList Class
-#
-#
-# class Node:
-#     def __init__(self, data=None, next_node=None):
-#         self.data = data
-#         self.next_node = next_node
-#
-#     def get_data(self):
-#         return self.data
-#
-#     def get_next(self):
-#         return self.next_node
-#
-#     def set_next(self, new_next):
-#         self.next_node = new_next
-#
-#
-# class LinkedList(object):
-#     def __init__(self, head=None):
-#         self.head = head
-#
-#     def insert(self, data):
-#         new_node = Node(data)
-#         new_node.set_next(self.head)
-#         self.head = new_node
-#
-#     def size(self):
-#         current_node = self.head
-#         count = 0
-#         while current_node:
-#             count += 1
-#             current_node = current_node.get_next()
-#         return count
-#
-#     def search(self, data):
-#         current_node = self.head
-#         found = False
-#         while current_node and found is False:
-#             if current_node.data == data:
-#                 found = True
-#             current_node = current_node.get_next()
-#
-#         if current_node is None and found is False:
-#             raise ValueError("not found")
-#
-#         return current_node
-#
-#     def delete(self, data):
-#         current_node = self.head
-#         previous_node = None
-#         found = False
-#         while current_node and found is False:
-#             if current_node.data == data:
-#                 found = True
-#             else:
-#                 previous_node = current_node
-#                 current_node = current_node.get_next()
-#
-#         if current_node is None and found is False:
-#             raise ValueError("not found")
-#
-#         if previous_node is None:
-#             self.head = current_node.get_next()
-#         else:
-#             previous_node.set_next(current_node.next_node)
-#
-#     def removeDups(self):
-#         l = []
-#         current_node = self.head
-#         previous_node = None
-#
-#         while current_node:
-#             if current_node.data in l:
-#                 previous_node.set_next(current_node.next_node)
-#                 current_node = current_node.get_next()
-#             else:
-#                 l.append(current_node.data)
-#
-#                 previous_node = current_node
-#                 current_node = current_node.get_next()
-# #
-# #
-#
-# class ListNodes:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-#
-#     def get_node(self, k):
-#         current = self.next
-#         i = 0
-#         while i < k:
-#             current = current.next
-#             i += 1
-#         return current
-#
-#     def insert_node(self, data):
-#         node = ListNode(data)
-#         node.next = self.next
-#         self.next = node
-#
-#
-# a = [2,3,4,5,10]
-# A = ListNodes(None)
-#
-# for i in a:
-#     A.insert_node(i)
-#
-# second_node = A.get_node(2)
-# print(second_node)
-# print(A)
-
-class Node:
-    def __init__(self, data=None):
-        self.val = data
-        self.next = None
-
-
-def insert(head, data):
-    node = Node(data)
-    node.next = head
-    head = node
-    return head
-
-
-def deleteDuplicates(A):
-    head = A
-    while A:
-        while A.next and A.next.val == A.val:
-            A.next = A.next.next
-        A = A.next
-    return head
-
-
-def delete_node(A, data):
-    head = A
-    if A is not None and A.val == data:
-        head = A.next
-        return head
-    while A:
-        if A.next is not None and A.next.val == data:
-            A.next = A.next.next
-        A = A.next
-
-    return head
 
 
 # Given a linked list, remove the n-th node from the end of list and return its head.
@@ -335,9 +303,7 @@ def lPalin_(A):
                 if l.pop() != s.pop():
                     return 0
         return 1
-
     else:
-
         return 0
 
 
@@ -362,7 +328,6 @@ def lPalin(A):
 # @return the first node in the cycle in the linked list
 def detectCycle(A):
     ls = {}
-    head = A
     if A is not None and A.val is not None:
         while A.next:
             if hash(A.val) in ls:
@@ -375,27 +340,6 @@ def detectCycle(A):
         return None
 
 
-# find size
-def size(head):
-    count = 0
-    while head:
-        count += 1
-        head = head.next
-    return count
-
-
-root = Node()
-# root = insert(root, 5)
-# root = insert(root, 4)
-# root = insert(root, 3)
-root = insert(root, 8)
-root = insert(root, 1)
-
-print(size(root))
 # root = removeNthFromEnd(root, 1)
 # root = lPalin(root)
-curr = root
-while curr.next:
-    print(curr.val)
-    curr = curr.next
-print(root)
+
--- str_manipulation.py
+++ str_manipulation.py
@@ -43,16 +43,16 @@ class Solution:
             return w.rstrip()
         else:
             return A
-
-    # @param A : string
-    # @param B : string
-    # @return an integer
-    # Implement strStr().strstr - locate a substring ( needle ) in a string ( haystack ).
-    # Try not to use standard library string functions for this question.
-    # Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
-    # Good clarification questions: What should be the return value if the needle is empty?
-    # What if both haystack and needle are empty?For the purpose of this problem,
-    # assume that the return value should be -1 in both cases.
+#
+#     # @param A : string
+#     # @param B : string
+#     # @return an integer
+#     # Implement strStr().strstr - locate a substring ( needle ) in a string ( haystack ).
+#     # Try not to use standard library string functions for this question.
+#     # Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+#     # Good clarification questions: What should be the return value if the needle is empty?
+#     # What if both haystack and needle are empty?For the purpose of this problem,
+#     # assume that the return value should be -1 in both cases.
     def strStr(self, A, B):
         if A:
             words = A.split(" ")
@@ -79,9 +79,122 @@ class Solution:
         else:
             return -1
 
+    def letterCombinations(self, A):
+        ls = {0: 0, 1: 1, 2: 'abc', 3: 'def', 4: 'ghi', 5: 'jkl', 6: 'mno', 7: 'pqrs', 8: 'tuv', 9: 'wxyz'}
+
+        # m = 'abc'  # ls[A[i]] = 2
+        # n = 'ghi'  # ls[A[i+1]] = 3
+        A.sort()
+        res = ['']
+        for k in A:
+            newRes = []
+            for i in res:
+                for j in ls[int(k)]:
+                    newRes.append(i + j)
+            res = newRes
+        return res
 
 sols = Solution()
-print(sols.countAndSay(7))
+print(sols.letterCombinations(list("23")))
+# class Solution:
+#     # @param A : list of integers
+#     # @param B : integer
+#     # @return a list of list of integers
+#     # Given candidate set 2,3,6,7 and target 7,
+#     # A solution set is:
+#     # [2, 2, 3]
+#     # [7]
+#     def combinationSum(self, A, t):
+#         ls = []
 
+#
+# def findSum(L, t, k, sum_, l):
+#     rst = 0
+#     while k < len(L):  # case 1: n * k = t
+#         sum_ += L[k]
+#         if sum_ == t:
+#             k += 1
+#             rst = 1
+#             yield l
+#
+#         if rst == 1:
+#             l = []
+#             sum_ = 0
+#             rst = 0
+#         l.append(L[k])
+#
+#         findSum(L, t, k, sum_, l)
+#
+#
+# A = [2,2,3]
+#
+# ls = []
+# for i in A:
+#     ls.append(list(findSum(A, 4, 0, 0, [])))
+#
+#
+#
+#
+# print(ls)
+
+
+# Python program to print all permutations with
+# duplicates allowed
+
+#
+# def toString(List):
+#     return ''.join(List)
+
+
+# Function to print permutations of string
+# This function takes three parameters:
+# 1. String
+# 2. Starting index of the string
+# 3. Ending index of the string.
+# def permute(a, l, r):
+#     if l == r:
+#         print(toString(a))
+#     else:
+#         for i in range(l, r + 1):
+#             a[l], a[i] = a[i], a[l]
+#             permute(a, l + 1, r)
+#             a[l], a[i] = a[i], a[l]  # backtrack
+
+# def permute(list, s):
+#     if list == 1:
+#         return s
+#     else:
+#         return [ y + x
+#                  for y in permute(1, s)
+#                  for x in permute(list - 1, s)
+#                  ]
+
+# print(permute(2, ["a","b","c"]))
+# # Driver program to test the above function
+# string = "ABC"
+# n = len(string)
+# a = list(string)
+# permute(a, 0, n-1)
+# sols = Solution()
+# print(sols.combinationSum([ 8, 10, 6, 11, 1, 16, 8 ], 28))
+# print(sols.countAndSay(7))
+
+# def permute(A, r, l, start):
+#
+#     if len(start) == len(A):
+#         print(start)
+#         start = ''
+#
+#     for i in range(r, l+1):
+#
+#         start += A[i]
+#
+#         permute(A, r+1, l, start)
+#
+#         A[r], A[l] = A[l], A[r]
+#
+# a=["a","b","c"]
+# n = len(a)
+# permute(a, 0, n-1, '')
 
 
